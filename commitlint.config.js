f6a7206666ca8c9e2645ec7b9c0ac49e6c3b2fe0
aibayanyu <aibayanyu@qq.com>
2022-07-30 12:03:44 +0800

    fix: commitlint support release

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,6 +3,7 @@ module.exports = {
     '@commitlint/config-conventional',
   ],
   rules: {
+    'type-enum': [2, 'always', ['feat', 'fix', 'style', 'ci', 'refactor', 'perf', 'test', 'build', 'chore', 'revert', 'release']],
     'subject-max-length': [1, 'always', 150],
   },
 }
